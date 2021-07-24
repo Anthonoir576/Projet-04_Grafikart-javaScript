090abcf7abcf92cd1570d6a7f9da9b46a693ba22
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-24 17:02:38 +0200

    debugge reussi

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,6 @@
 // class
+
+// THIS REF : #CAROUSEL1, #CAROUSEL2, ou #CAROUSEL3
 class Carousel {
 
     /*
@@ -17,13 +19,24 @@ class Carousel {
             slidesToScroll: 1,
             slidesVisible: 1
 
-        }, options)
+        }, options),
+
+        // permet de recupere les elements enfants item a ce moment du script sinon le carousel, et caoursel__container aurait etait considéré comme element enfant
+        this.children = [].slice.call(element.children)
 
         let root = this.createDivWithClass('carousel');
         let container = this.createDivWithClass('carousel__container');
         
         root.appendChild(container);
         this.element.appendChild(root);
+
+        this.children.forEach( (child) => {
+            
+            let item = this.createDivWithClass('carousel__item');
+            item.appendChild(child)
+            container.appendChild(item);
+
+        });
 
     };
 
