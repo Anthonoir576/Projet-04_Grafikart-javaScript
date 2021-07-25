e395ce47fb90091e126355dc244a38287281b6e7
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-25 11:00:23 +0200

    debug ras

--- source/js/index.js
+++ source/js/index.js
@@ -19,35 +19,72 @@ class Carousel {
             slidesToScroll: 1,
             slidesVisible: 1
 
-        }, options),
+        }, options)
 
-        // permet de recupere les elements enfants item a ce moment du script sinon le carousel, et caoursel__container aurait etait considéré comme element enfant
-        this.children = [].slice.call(element.children)
-
-        let root = this.createDivWithClass('carousel');
-        let container = this.createDivWithClass('carousel__container');
         
-        root.appendChild(container);
-        this.element.appendChild(root);
+        // permet de recupere les elements enfants item a ce moment du script sinon le carousel, et caoursel__container aurait etait considéré comme element enfant
+        let children = [].slice.call(element.children);
+        this.root = this.createDivWithClass('carousel');
 
-        this.children.forEach( (child) => {
+        this.container = this.createDivWithClass('carousel__container');
+        
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
+
+        this.items = children.map( (child) => {
             
             let item = this.createDivWithClass('carousel__item');
-            item.appendChild(child)
-            container.appendChild(item);
+            
+            item.appendChild(child);
+            this.container.appendChild(item);
+
+            return item
 
         });
+
+        this.setStyle();
+        this.createNavigation();
+
+    };
+
+    /**
+     * 
+     * Applique les bonnes dimension au carousel
+     */
+    setStyle () {
+
+        let ratio = this.items.length / this.options.slidesVisible;
+        this.container.style.width = (ratio * 100) + "%";
+        this.items.forEach(item => {item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%"});
+
+    };
+
+    createNavigation () {
+
+        let nextButton = this.createDivWithClass('carousel__next');
+        let prevButton = this.createDivWithClass('carousel__prev');
+
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this));
 
     };
 
 
+    /**
+     * 
+     * @param retourne une div avec une class
+     *
+     */
     createDivWithClass (className) {
 
         let div = document.createElement('div');
         div.setAttribute('class', className)
         return div
 
-    }
+    };
 
 };
 
